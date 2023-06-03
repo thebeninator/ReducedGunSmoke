ed534135c0c87238d788395a1ed640b5582084d3
ATLAS <ATLAS@DESKTOP-IDR5L8E>
2023-06-02 22:02:54 -0700

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using ReducedGunSmoke;
+
+[assembly: MelonInfo(typeof(ReducedGunSmokeMod), "Reduced Gun Smoke", "1.0.0", "ATLAS")]
+[assembly: MelonGame("Radian Simulations LLC", "GHPC")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReducedGunSmoke")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReducedGunSmoke")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64b43337-3d8f-4cf8-b795-cc10f0ddd7ae")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReducedGunSmoke.cs
@@ -0,0 +1,65 @@
+﻿using GHPC.Player;
+using GHPC.Weapons;
+using MelonLoader;
+using UnityEngine;
+
+namespace ReducedGunSmoke
+{
+    public class ReducedGunSmokeMod : MelonMod
+    {
+        GameObject mission;
+        GameObject currentDustEmitter;
+        WeaponSystem currentPlayerWeapon;
+        int currentPlayerWeaponId;
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            currentPlayerWeapon = null;
+            currentDustEmitter = null;
+            currentPlayerWeaponId = 0;
+            mission = GameObject.Find("_APP_GHPC_");
+        }
+
+        public override void OnUpdate()
+        {
+            if (mission == null) 
+                return;
+
+            if (mission.GetComponent<PlayerInput>() == null)
+                return; 
+
+            if (mission.GetComponent<PlayerInput>().CurrentPlayerWeapon == null)
+                return;
+            
+            PlayerInput player = mission.GetComponent<PlayerInput>();
+            WeaponSystem weapon = player.CurrentPlayerWeapon.Weapon;
+            int weaponId = weapon.GetInstanceID();
+
+            if (currentPlayerWeaponId == 0)
+            {
+                currentPlayerWeapon = weapon;
+                currentPlayerWeaponId = weaponId;
+            }
+
+            if (currentPlayerWeaponId != weaponId) {
+                if (currentDustEmitter != null)
+                    currentDustEmitter.SetActive(true);
+
+                currentDustEmitter = null;
+                currentPlayerWeapon = null;
+                currentPlayerWeaponId = 0;
+            }
+
+            try
+            {
+                ParticleSystem[] particles = currentPlayerWeapon.MuzzleEffects;
+                currentDustEmitter = particles[0].gameObject.transform.Find("Particle System").gameObject;
+                currentDustEmitter.SetActive(false);
+            }
+            catch {
+
+            }
+        }
+    }   
+}
+
